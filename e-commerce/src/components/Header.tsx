0eaedddee9ca2ba2c4f32f960c56b5a0f0654a94
DhruviShah16 <19dce123@charusat.edu.in>
2023-05-28 17:05:55 +0530

    menu

--- e-commerce/src/components/Header.tsx
+++ e-commerce/src/components/Header.tsx
@@ -25,7 +25,7 @@ const Header = () => {
     if (isUser === null) {
       // const response = await signInWithPopup(firebaseAuth, provider): This line attempts to sign in the user using a popup window provided by Firebase Authentication. It calls the signInWithPopup() function, passing in the firebaseAuth instance (Firebase Authentication service) and the provider instance (Google authentication provider).
       const response = await signInWithPopup(firebaseAuth, provider);
-      console.log(response);
+   
       setUser(response);
     }
     setIsMenu(!isMenu);
@@ -82,13 +82,13 @@ const Header = () => {
             {/* here if user is not loggedIn then display a avatar img else display user's profile */}
             <motion.img
               whileTap={{ scale: 0.6 }}
-              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+              className="w-10 min-w-[40px] h-10 min-h-[40px] cursor-pointer rounded-full"
               src={isUser ? photo! : "/assets/avatar.png"}
               alt="user"
-              onClick={isUser ? login : signIn}
+              onClick={signIn}
             />
-            {isMenu && (
-              <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute px-4 py-2 top-12 right-0">
+            {isUser && isMenu && (
+              <div className="w-40 bg-gray-50 shadow-2xl  rounded-lg flex flex-col absolute px-4 py-2 top-12 right-0">
                 {/* if user is admin here I am admin with my e-mail id then only allow to create new item for adding */}
                 {isUser?.user.email === "dhruvikalpesh2001@gmail.com" && (
                   <Link to="/createItem">
@@ -109,31 +109,26 @@ const Header = () => {
               </div>
             )}
             {!isUser && isMenu && (
-              <ul className="flex gap-8">
-                <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute px-4 py-2 top-12 -right-12">
+              <ul>
+                <div className="w-64 bg-gray-50 shadow-2xl shadow-black rounded-lg flex flex-col absolute px-4 py-2 top-12 -right-12 gap-3">
                   {/* if user is admin here I am admin with my e-mail id then only allow to create new item for adding */}
 
                   <li
-                    className="px-4 py-2 flex items-center gap-3 cursor-pointer bg-gray-200 justify-center rounded-md shadow-md hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base"
-                    onClick={logout}
+                    className="py-2 flex items-center gap-3 cursor-pointer bg-gray-200 justify-center rounded-md shadow-md hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base"
+                    onClick={login}
                   >
-                    LogIn With Same account
+                    Login With Same account
                     <MdLogout />
                   </li>
 
                   <li
-                    className="px-4 py-2 flex items-center gap-3 cursor-pointer bg-gray-200 justify-center rounded-md shadow-md hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base"
-                    onClick={logout}
+                    className="px-2 py-2 flex items-center gap-3 cursor-pointer bg-gray-200 justify-center rounded-md shadow-md hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base"
+                    // onClick={logout}
                   >
-                    LogIn With different Account <MdLogout />
+                    Login With different Account <MdLogout />
                   </li>
 
-                  <li
-                    className="px-4 py-2 flex items-center gap-3 cursor-pointer bg-gray-200 justify-center rounded-md shadow-md hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base"
-                    onClick={logout}
-                  >
-                    Logout <MdLogout />
-                  </li>
+               
                 </div>
               </ul>
             )}{" "}
@@ -148,29 +143,32 @@ const Header = () => {
             <p className="text-xs text-white font-semibold">0</p>
           </div>
         </div>
-        <div className="flex gap-2">
-          <img
-            className="w-14 object-cover rounded-full"
-            src="/assets/logo.png"
-            alt=""
-          />
-        </div>
+
+       
+          <Link to={"/"} className="flex  gap-2">
+            <img
+              className="w-14 object-cover rounded-full"
+              src="/assets/logo.png"
+              alt="logo"
+            />
+          </Link>
+      
 
         <div className="relative">
           {/* here if user is not loggedIn then display a avatar img else display user's profile */}
           <motion.img
             whileTap={{ scale: 0.6 }}
-            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+            className="w-10 min-w-[40px] h-10 min-h-[40px] cursor-pointer rounded-full"
             src={isUser ? photo! : "/assets/avatar.png"}
             alt="user"
-            onClick={isUser ? login : signIn}
+            onClick={signIn}
           />
-          {isMenu && (
+          {isUser && isMenu && (
             <ul className="flex gap-8">
               <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute px-4 py-2 top-12 -right-12">
                 {/* if user is admin here I am admin with my e-mail id then only allow to create new item for adding */}
 
-                {isUser?.user.email === "dhruvikalpesh2001@gmail.com" && (
+                {isUser.user.email === "dhruvikalpesh2001@gmail.com" && (
                   <Link to="/createItem">
                     <li className="py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
                       New Item <MdAdd />
@@ -197,43 +195,33 @@ const Header = () => {
                   className="px-4 py-2 flex items-center gap-3 cursor-pointer bg-gray-200 justify-center rounded-md shadow-md hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base"
                   onClick={logout}
                 >
-                  LogIn <MdLogout />
+                  LogOut <MdLogout />
                 </li>
               </div>
             </ul>
           )}{" "}
-          {/* {!isUser && isMenu && (
+          {!isUser && isMenu && (
             <ul className="flex gap-8">
-              <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute px-4 py-2 top-12 -right-12"> */}
+              <div className="w-64 bg-gray-50 shadow-2xl shadow-black rounded-lg flex flex-col absolute px-4 py-2 top-12 -right-10 gap-3 ">
                 {/* if user is admin here I am admin with my e-mail id then only allow to create new item for adding */}
 
-                {/* <li
+                <li
                   className="px-4 py-2 flex items-center gap-3 cursor-pointer bg-gray-200 justify-center rounded-md shadow-md hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base"
-                  onClick={logout}
+                  onClick={login}
                 >
-                  LogIn With Same account
+                  Login With Same account
                   <MdLogout />
                 </li>
 
                 <li
                   className="px-4 py-2 flex items-center gap-3 cursor-pointer bg-gray-200 justify-center rounded-md shadow-md hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base"
-                  onClick={logout}
+                  // onClick={logout}
                 >
-                  LogIn With different Account <MdLogout />
-                </li>
-
-                <li
-                  className="px-4 py-2 flex items-center gap-3 cursor-pointer bg-gray-200 justify-center rounded-md shadow-md hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base"
-                  onClick={logout}
-                >
-                  Logout <MdLogout />
+                  Login With different Account <MdLogout />
                 </li>
               </div>
             </ul>
-          )} */}
-          
-          
-          {" "}
+          )}{" "}
         </div>
       </div>
     </header>
